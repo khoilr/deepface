68c9d88f622d27c111983c7a0c442cc46c1361f2
vinhpna1c <vinhpna@1cinnovation.com>
2023-09-25 15:35:58 +0700

    add get image path

---
+++ camera_web/images/views.py
@@ -0,0 +1,20 @@
+import pathlib
+
+from django.http import FileResponse
+from django.shortcuts import render
+
+# Create your views here.
+
+def getImage(request,slug):
+    print(slug)
+    cam_dir_parent = pathlib.Path.cwd()
+
+    image_path = cam_dir_parent.joinpath('images/frames/'+slug)
+    print(image_path)
+    img = open(image_path , 'rb')
+    respond = FileResponse(img)
+
+    return respond
+
+def test_camera(request):
+    return render(request,"home.html")
